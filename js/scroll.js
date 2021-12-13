fab17969533374cbfe4db48694dd75f687ca3746
AndyR <marlon_rivera95@hotmail.com>
2021-12-13 15:38:12 -0500

    se agrega scrollreveal

---
+++ js/scroll.js
@@ -0,0 +1,13 @@
+ScrollReveal().reveal('.titulo__ofrecemos', { delay: 200 });
+
+ScrollReveal().reveal('.cli1', { delay: 300 });
+ScrollReveal().reveal('.cli2', { delay: 400 });
+ScrollReveal().reveal('.cli3', { delay: 500 });
+
+window.sr = ScrollReveal();
+
+    sr.reveal('.iconos__ofrecemos' , {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '50px'
+    });
