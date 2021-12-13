57914777c118c2ee60fbefac875bf5102cfecd9e
AndyR <marlon_rivera95@hotmail.com>
2021-12-13 15:46:35 -0500

    se agrega scrollreveal a mas elementos

--- js/scroll.js
+++ js/scroll.js
@@ -1,5 +1,9 @@
 ScrollReveal().reveal('.titulo__ofrecemos', { delay: 200 });
+ScrollReveal().reveal('.header__acomodar', { delay: 300 });
+ScrollReveal().reveal('.container__footer', { delay: 300 });
 
+
+ScrollReveal().reveal('.titulo__clientas', { delay: 300 });
 ScrollReveal().reveal('.cli1', { delay: 300 });
 ScrollReveal().reveal('.cli2', { delay: 400 });
 ScrollReveal().reveal('.cli3', { delay: 500 });
@@ -11,3 +15,14 @@ window.sr = ScrollReveal();
         origin: 'bottom',
         distance: '50px'
     });
+
+
+
+    window.sr = ScrollReveal();
+
+    sr.reveal('.container__grid' , {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '50px'
+    });
+
